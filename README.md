b11e902053cc803bfe17170a74c54fb934124832
Treeek <johngc2010@hotmail.com>
2018-09-28 23:59:48 -0300

    estrutura básica do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# WantOrganizer
-Programinha que eu fiz pra automatizar a organização da minha want de cartas de magic usando a API do scryfall.
---
+++ index.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+const urlScryfall = "https://api.scryfall.com/cards/named?exact=";
+const entrada = [{
+	nome: "Island",
+	quantidade: 1,
+}, {
+	nome: "Swamp",
+	quantidade: 1,
+}, {
+	nome: "Lightning Bolt",
+	quantidade: 1,
+}];
+
+
